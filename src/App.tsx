ee6e51439a50676a195b9c5e15e2d97af4f7f335
Joshua Rogan <joshua.rogan@mydit.ie>
2021-02-08 16:17:08 +0000

    simplified some code stuff, understand sdk a bit better

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,23 @@
 import React from 'react';
 import './App.css';
+import {CodePipelineClient, ListPipelinesCommand} from '@aws-sdk/client-codepipeline'
 import AWS from 'aws-sdk';
 
 
 
 function App() {
 
+const codePipelineClient = new CodePipelineClient({
+    region: 'eu-west-1',
+    credentials: {
+       accessKeyId: process.env.REACT_APP_ACCESS_KEY_ID!,
+       secretAccessKey: process.env.REACT_APP_SECRET_ACCESS_KEY!
+}})
 
-const codepipeline = new AWS.CodePipeline({
-  region: 'eu-west-1',
-  credentials: {
-    accessKeyId: process.env.REACT_APP_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.REACT_APP_SECRET_ACCESS_KEY!
-  }
-})
+let results = codePipelineClient.send(new ListPipelinesCommand({}))
 
-console.log('secrets', process.env.REACT_APP_ACCESS_KEY_ID)
-console.log('secrets2', process.env.REACT_APP_SECRET_ACCESS_KEY)
-console.log(codepipeline)
-
-let test =  codepipeline.listPipelines({}).promise().then(function(data) {
-  return data
-}).catch(function(err) {
-  return err
-}).then(async resp => {
-  console.log(resp)
-})
-
-
-
-let trying = codepipeline.listPipelines({}).promise().then(function(data) {
-  let pipelines = data.pipelines
-  if (!pipelines) {
-    return null
-
-  }
-  let names = pipelines.map( (pipeline) => {
-    console.log(pipeline.name)
-    return pipeline.name
-  }
-  )
-
-  return names
-}
-).catch(err => {
-  console.log('err', err)
-}).then(res => {
-  console.log('finalres',res)
+results.then(res => {
+  console.log(res)
 })
 
 
