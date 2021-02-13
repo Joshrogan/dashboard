2391ae69e1426c2e39f6d44cd4ba793a608ade3a
Joshua Rogan <joshua.rogan@mydit.ie>
2021-02-13 12:46:38 +0000

    added time library

--- src/components/Pipeline.tsx
+++ src/components/Pipeline.tsx
@@ -3,6 +3,7 @@ import {PipelineModel} from '../api/CodePipelineModels'
 import Card from '@material-ui/core/Card';
 import { makeStyles } from '@material-ui/core/styles';
 import CardContent from '@material-ui/core/CardContent';
+import ReactTimeAgo from 'react-time-ago'
 
 
 
@@ -34,12 +35,20 @@ const Pipeline: React.FC<PipelineProps> = ({pipeline}: PipelineProps) => {
         return null 
     }
 
+    let lastUpdateTime: any = null;
+    if (pipeline?.pipelineExecutionSummary !== undefined) {
+      lastUpdateTime = pipeline.pipelineExecutionSummary[0].lastUpdateTime ? pipeline.pipelineExecutionSummary[0].lastUpdateTime : null
+    }
+
     console.log('pipelines', pipeline)
 
     return (
         <div className={classes.root}>
         <Card className={classes.card}>
           <CardContent>{pipeline.pipelineName}</CardContent>
+          <CardContent>
+            <ReactTimeAgo date={lastUpdateTime}/>
+            </CardContent>
         </Card>
         </div>
     )
