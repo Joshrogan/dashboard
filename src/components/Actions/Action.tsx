7b1e00e1e67e0c13ca1671781883bbf7747b3ffc
Joshua Rogan <joshua.rogan@mydit.ie>
2021-02-15 11:39:49 +0000

    changed Action signature

--- src/components/Actions/Action.tsx
+++ src/components/Actions/Action.tsx
@@ -2,31 +2,11 @@ import BuildAction from './BuildAction';
 import DeployAction from './DeployAction';
 import SourceAction from './SourceAction';
 import { ActionModel } from '../../api/CodePipelineModels';
-import { ActionTypeId } from '@aws-sdk/client-codepipeline';
-
-// interface JsxElementArray {
-//     [index: string]: JSX.Element
-// }
-
-// let myArray: JsxElementArray = (action: ActionModel) => ({
-//     Build: <BuildAction action={action}/>,
-//     "Deploy": <DeployAction action={action} />,
-//     "Source": <SourceAction action={action} />
-// })
-
-// const allTypes: JSX.Element = (action: ActionModel) =>
-//     Build: <BuildAction action={action}/>,
-//     Deploy: <DeployAction action={action} />,
-//     Source: <SourceAction action={action} />
-
-// const getAction = (action: ActionModel) => ({
-//     allTypes(action)
-// })
 
 type ActionProps = {
   action: ActionModel | undefined;
 };
-const Action: any | undefined = (Props: ActionProps) => {
+const Action: Function = (Props: ActionProps) => {
   let category = Props.action?.category;
   let action = Props.action;
   switch (category) {
