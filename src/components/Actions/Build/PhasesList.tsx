b7eb3e4614156f226afdae50889f618982e86ad2
Joshua Rogan <joshua.rogan@mydit.ie>
2021-02-22 15:42:06 +0000

    basic READ dashboard finished

--- src/components/Actions/Build/PhasesList.tsx
+++ src/components/Actions/Build/PhasesList.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import ListSubheader from '@material-ui/core/ListSubheader';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import Collapse from '@material-ui/core/Collapse';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import { getStatusColor } from '../../pipelineUtils';
 import ReactTimeAgo from 'react-time-ago';
 import { PhaseModel } from '../../../api/CodeBuildModels';
 import { renderToString } from 'react-dom/server';
 import { Typography } from '@material-ui/core/';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
 
 type PhasesListProps = {
   phaseList: PhaseModel[];
@@ -66,40 +64,20 @@ const getListTextSecondary = (phase: PhaseModel) => {
 };
 
 const PhasesList: React.FC<PhasesListProps> = ({ phaseList }: PhasesListProps) => {
-  const [open, setOpen] = React.useState(false);
-
-  const handleClick = () => {
-    setOpen(!open);
-  };
-
   return (
-    <List
-      component="nav"
-      aria-labelledby="nested-list-subheader"
-      subheader={
-        <ListSubheader component="div" id="nested-list-subheader">
-          Phases
-        </ListSubheader>
-      }
-    >
-      <ListItem button onClick={handleClick}>
-        <ListItemIcon>
-          <InboxIcon />
-        </ListItemIcon>
-        <ListItemText primary="Click to expand phase list" />
-        {open ? <ExpandLess /> : <ExpandMore />}
-      </ListItem>
-      <Collapse in={open} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding>
-          {phaseList.map((phase) => (
-            <ListItem button>
-              <ListItemIcon>{getStatusIcon(phase.phaseStatus)}</ListItemIcon>
-              <ListItemText secondary={getListTextSecondary(phase)}>{getListItemText(phase)}</ListItemText>
-            </ListItem>
-          ))}
-        </List>
-      </Collapse>
-    </List>
+    <Accordion>
+      <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1b-content" id="panel1b-header">
+        Phase Details
+      </AccordionSummary>
+      <List component="div" disablePadding>
+        {phaseList.map((phase) => (
+          <ListItem button>
+            <ListItemIcon>{getStatusIcon(phase.phaseStatus)}</ListItemIcon>
+            <ListItemText secondary={getListTextSecondary(phase)}>{getListItemText(phase)}</ListItemText>
+          </ListItem>
+        ))}
+      </List>
+    </Accordion>
   );
 };
 
