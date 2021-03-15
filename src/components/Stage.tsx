4560c3261e119a5cf86252c508d92adcaebd5ed9
Joshua Rogan <joshua.rogan@mydit.ie>
2021-03-15 20:15:13 +0000

    updated stage icon logic

--- src/components/Stage.tsx
+++ src/components/Stage.tsx
@@ -8,18 +8,70 @@ import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import { TreeItem } from '@material-ui/lab';
 import CheckIcon from '@material-ui/icons/Check';
 import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
-import { green, red } from '@material-ui/core/colors';
+import BlurCircularIcon from '@material-ui/icons/BlurCircular';
+import { green, red, blue } from '@material-ui/core/colors';
 import { Button } from '@material-ui/core';
 import { CodePipelineService } from '../api/CodePipelineService';
-
+import { PipelineExecutionStatus } from '@aws-sdk/client-codepipeline';
+import { StatusType } from '@aws-sdk/client-codebuild';
 type StageProps = {
   stage: StageModel;
   pipeline: PipelineModel;
   pipelineClient: CodePipelineService;
 };
 
+const getStageLabel = (
+  status: string,
+  pipelineName: string,
+  stageName: string,
+  pipelineExecutionId: string,
+  pipelineClient: CodePipelineService
+): any => {
+  let retry = (
+    <Button
+      color="secondary"
+      variant="outlined"
+      onClick={() => pipelineClient.retryStageExecution(pipelineName, stageName, pipelineExecutionId!)}
+    >
+      Retry stage
+    </Button>
+  );
+
+  switch (status) {
+    case PipelineExecutionStatus.Failed:
+    case StatusType.FAILED:
+    case StatusType.FAULT:
+    case PipelineExecutionStatus.Stopped:
+    case StatusType.STOPPED:
+    case PipelineExecutionStatus.Stopping:
+    case StatusType.TIMED_OUT:
+    case PipelineExecutionStatus.Superseded:
+      return (
+        <>
+          <ErrorOutlineIcon style={{ color: red[500] }} />
+          &nbsp;
+          {retry}
+        </>
+      );
+    case PipelineExecutionStatus.InProgress:
+    case StatusType.IN_PROGRESS:
+      return <BlurCircularIcon style={{ color: blue[500] }} />;
+    case PipelineExecutionStatus.Succeeded:
+    case StatusType.SUCCEEDED:
+      return <CheckIcon style={{ color: green[500] }} />;
+    default:
+      return null;
+  }
+};
+
 const Stage: React.FC<StageProps> = ({ stage, pipeline, pipelineClient }: StageProps) => {
-  if (stage === undefined || pipeline === undefined || stage.actions === undefined) {
+  if (
+    stage === undefined ||
+    pipeline === undefined ||
+    stage.actions === undefined ||
+    stage.status === undefined ||
+    pipeline.pipelineExecutionId === undefined
+  ) {
     return null;
   }
 
@@ -27,24 +79,13 @@ const Stage: React.FC<StageProps> = ({ stage, pipeline, pipelineClient }: StageP
 
   let stageName = `Stage: ${stage.stageName}`;
 
-  let icon =
-    stage.status === 'Succeeded' ? (
-      <CheckIcon style={{ color: green[500] }} />
-    ) : (
-      <>
-        <ErrorOutlineIcon style={{ color: red[500] }} />
-        &nbsp;
-        <Button
-          color="secondary"
-          variant="outlined"
-          onClick={() =>
-            pipelineClient.retryStageExecution(pipeline.pipelineName, stage.stageName, pipeline.pipelineExecutionId!)
-          }
-        >
-          Retry stage
-        </Button>
-      </>
-    );
+  let icon = getStageLabel(
+    stage.status,
+    pipeline.pipelineName,
+    stage.stageName,
+    pipeline.pipelineExecutionId,
+    pipelineClient
+  );
 
   let stageLabel = (
     <div
